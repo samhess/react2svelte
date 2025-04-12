ae95b42f097b4c41c42bb5eee7196a7a16be3497
Samuel Hess <sam.hess@me.com>
2025-04-12 13:40:31 +0300

    add scripts/react2svelte.js

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# react2svelte
-Simple React to Svelte converter
+# sv
+
+Everything you need to build a Svelte project, powered by [`sv`](https://github.com/sveltejs/cli).
+
+## Creating a project
+
+If you're seeing this, you've probably already done this step. Congrats!
+
+```bash
+# create a new project in the current directory
+npx sv create
+```
+
+## Converting a React Router project
+
+```bash
+cd scripts
+node react2svelte.js <reactRepo> <route>
+npm run format
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
---
+++ scripts/react2svelte.js
@@ -0,0 +1,94 @@
+import { existsSync, mkdirSync } from 'fs'
+import { readFile, readdir, writeFile } from 'fs/promises'
+import { basename, format, resolve } from 'path'
+
+const reactRepoName = process.argv[2] ?? 'reactRepo'
+const reactRepo = resolve(import.meta.dirname, '..', '..', reactRepoName)
+const route = process.argv[3] ?? ''
+
+function getImports(code) {
+	return code
+		.split('\r\n')
+		.filter((line) => line.includes('import ') && !line.includes('import type {Route} from'))
+		.map((line) => line.trim())
+		.join('\r\n')
+		.replace(/@\/lib/g, '$lib')
+		.replace(/react-router/g, '@sveltejs/kit')
+}
+
+function getScript(code) {
+	const startFunction = code.indexOf('export default function')
+	const startFunctionBody = code.indexOf('\r\n',startFunction)
+	const endScript = code.indexOf('return', startFunctionBody)
+	return code.slice(startFunctionBody,endScript)
+		.replaceAll(/loaderData/g, '$props')
+		.replaceAll(/(\w*) = useState/g, '$1 = $state')
+}
+
+function getTemplate(code) {
+	const startFunction = code.indexOf('export default function');
+	const startReturn = code.indexOf('return', startFunction);
+	code = code.slice(code.indexOf('(', startReturn) + 1, code.lastIndexOf(')'));
+	return (
+		code
+			.replaceAll('<>', '')
+			.replaceAll('</>', '')
+			.replaceAll('className=', 'class=')
+			.replaceAll('defaultValue', 'bind:value')
+			.trim()
+	);
+}
+
+function getLoader(code) {
+	const start = code.indexOf('export async function loader');
+	const end = code.indexOf('export default function');
+	code = code.slice(start, end);
+	return code.replace(/loader\(([^)]*)\)/g, 'load({params,request})');
+}
+
+function createTemplate(reactCode) {
+	const script = getScript(reactCode);
+	const template = getTemplate(reactCode);
+
+	return `
+<script>
+${script.trim()}
+</script>
+
+${template.trim()}
+`
+}
+
+function createLoader(reactCode) {
+	const imports = getImports(reactCode);
+	const loader = getLoader(reactCode);
+
+	return `
+${imports.trim()}
+
+${loader.trim()}
+`
+}
+
+const files = await readdir(resolve(reactRepo, 'app', 'routes', route), {
+	withFileTypes: true,
+	recursive: true
+});
+for (const file of files) {
+	if (!file.isDirectory()) {
+		const childRoute = basename(file.name, '.tsx');
+		const reactCode = await readFile(resolve(file.parentPath, file.name), { encoding: 'utf-8' });
+		const loader = createLoader(reactCode);
+		const tempalte = createTemplate(reactCode);
+		const routePath = resolve(import.meta.dirname, '..', 'src', 'routes', route, childRoute);
+		const loaderFile = format({ name: '+page', ext: 'server.ts', dir: routePath });
+		const templateFile = format({ name: '+page', ext: 'svelte', dir: routePath });
+		if (!existsSync(routePath)) {
+			mkdirSync(routePath, { recursive: true });
+		}
+		await writeFile(loaderFile, loader);
+		await writeFile(templateFile, tempalte);
+		console.log(`${loaderFile} generated`);
+		console.log(`${templateFile} generated`);
+	}
+}
